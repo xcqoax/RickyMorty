bf91defdde7aa4002f5f9fe827cf25558ca65563
Gonzalo Peralta <palmer_85@hotmail.com>
2020-10-07 02:24:36 -0500

    Updates

---
+++ precache-manifest.87bd8bd59180889d82aa5ed262085dcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce001f6b071a6b7381a46a44d58a574",
+    "url": "/RickyMorty/index.html"
+  },
+  {
+    "revision": "9e211c1faf2b02919fa0",
+    "url": "/RickyMorty/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "42481d394c368663bdef",
+    "url": "/RickyMorty/static/css/main.d1c2cc9c.chunk.css"
+  },
+  {
+    "revision": "9e211c1faf2b02919fa0",
+    "url": "/RickyMorty/static/js/2.a327cab1.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/RickyMorty/static/js/2.a327cab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42481d394c368663bdef",
+    "url": "/RickyMorty/static/js/main.ab7fb439.chunk.js"
+  },
+  {
+    "revision": "413956686a129f0c4d8b",
+    "url": "/RickyMorty/static/js/runtime-main.47d545ea.js"
+  }
+]);
